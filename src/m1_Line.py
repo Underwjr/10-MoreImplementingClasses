4e7eff640823ad8743125cd6e197599029a3692d
underwjr <underwjr@rose-hulman.edu>
2019-03-08 17:25:43 -0500

    MoreImplementingClasses 10 m1 Done

--- src/m1_Line.py
+++ src/m1_Line.py
@@ -719,6 +719,7 @@ class Line(object):
         self.start = self.first_start
         self.end = self.first_end
 
+
         # ---------------------------------------------------------------------
         # Done 13.
         #   a. READ the above specification, including the Example.
